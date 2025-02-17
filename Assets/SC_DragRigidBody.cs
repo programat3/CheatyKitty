7f63dfa7ae2728959533b1174991948fe3c31298
Lía Da Silva Rojas <lia.dasilva.info@gmail.com>
2025-02-16 21:23:29 -0300

    Primer Commit

---
+++ Assets/SC_DragRigidBody.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class SC_DragRigidbody2D : MonoBehaviour
+{
+    public float forceAmount = 500f;
+
+    Rigidbody2D selectedRigidbody;
+    Camera targetCamera;
+    Vector2 originalScreenTargetPosition;
+    Vector2 originalRigidbodyPos;
+    void Start()
+    {
+        targetCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (!targetCamera)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+        
+            selectedRigidbody = GetRigidbodyFromMouseClick();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectedRigidbody.velocity = Vector2.zero;
+            selectedRigidbody = null;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (selectedRigidbody)
+        {
+            Vector2 mousePosition = targetCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePositionOffset = mousePosition - originalScreenTargetPosition;
+            Vector2 vel = (originalRigidbodyPos +mousePositionOffset - (Vector2)selectedRigidbody.transform.position) *forceAmount * Time.deltaTime;
+            selectedRigidbody.velocity = vel;
+
+        }
+    }
+
+    Rigidbody2D GetRigidbodyFromMouseClick()
+    {
+        Vector2 mousePosition = targetCamera.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hitInfo = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.attachedRigidbody != null)
+            {
+                originalScreenTargetPosition = mousePosition;
+                originalRigidbodyPos = hitInfo.collider.transform.position;
+                return hitInfo.collider.attachedRigidbody;
+            }
+        }
+
+        return null;
+    }
+}
